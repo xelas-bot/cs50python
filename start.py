f1fea87913d7ec2872aededb343ae7e5e2b51f40
Shrey Patel <shreyp941@gmail.com>
2020-05-25 10:34:11 -0500

    hahaah

---
+++ start.py
@@ -0,0 +1,16 @@
+import discord
+import random
+
+
+def myFunc():
+    x=random.randrange(1, 10)
+
+    if 6>2:
+        print(x)
+    else:
+        print(x)
+
+
+myFunc()
+
+print(int("3") + int("5"))
